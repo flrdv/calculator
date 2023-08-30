e0b3c861fda739ec6cf89ad1ff69150ecb0142d3
A Really Fake Floordiv Since 4th January, 2022 <97134096+fakefloordiv@users.noreply.github.com>
2023-08-30 11:31:06 +0200

    added repl, prettified the code

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"calculator/frontend/lex"
 	"calculator/frontend/parse"
+	"calculator/interpret"
 	"fmt"
+	"os"
 )
 
+func input(prompt string) string {
+	fmt.Print(prompt)
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+
+	return text
+}
+
+func repl() error {
+	const prompt = "> "
+	interpreter := interpret.NewInterpreter(map[string]float64{
+		"x": 5,
+	})
+
+	for {
+		expr := input(prompt)
+		if err := calculate(interpreter, expr); err != nil {
+			fmt.Println("error:", err)
+		}
+	}
+}
+
+func calculate(interpreter interpret.Interpreter, expr string) error {
+	ast, err := parse.NewParser(lex.NewLexer(expr)).Parse()
+	if err != nil {
+		return err
+	}
+
+	for _, branch := range ast {
+		result, err := interpreter.Execute(branch)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(result)
+	}
+
+	return nil
+}
+
 func main() {
-	const code = "2*8-6+5"
-	lexer := lex.NewLexer(code)
-	parser := parse.NewParser(lexer)
-	fmt.Println(parser.Parse())
+	//fmt.Println(calculate(interpret.NewInterpreter(nil), "-(2+2)*x"))
+	fmt.Println("repl:", repl())
 }

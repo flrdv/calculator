26a69380e46f52dbce353cc61745cbefe486d6d9
A Really Fake Floordiv Since 4th January, 2022 <97134096+fakefloordiv@users.noreply.github.com>
2023-08-31 00:02:16 +0200

    moved ast to its own sub-package
    now it is contained at the ast package, that is a subpackage of parse

---
+++ frontend/parse/ast/ast.go
@@ -0,0 +1,27 @@
+package ast
+
+import (
+	"calculator/frontend/lex"
+)
+
+type Program []Node
+
+type (
+	Node    any
+	Integer = int64
+	Float   = float64
+	ID      = string
+	BinOp   struct {
+		Op          lex.LexemeType
+		Left, Right Node
+	}
+	UnOp struct {
+		Op    lex.LexemeType
+		Value Node
+	}
+	Function = func(...Node) (Node, error)
+	FCall    struct {
+		Target Node
+		Args   []Node
+	}
+)

50405c2e4b0ac408294250fe282cf75f039b1cba
fakefloordiv <real.floordiv@gmail.com>
2023-09-01 08:51:37 +0200

    improved tests
    formatted them in a better way, made named struct initializers

--- frontend/parse/parser_test.go
+++ frontend/parse/parser_test.go
@@ -3,41 +3,63 @@ package parse
 import (
 	"calculator/frontend/lex"
 	"calculator/frontend/parse/ast"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestParser(t *testing.T) {
 	tcs := []struct {
+		Name string
 		Expr string
-		AST  ast.Node
+		Want ast.Node
 	}{
-		{"42", ast.Integer(42)},
-		{"abc", "abc"},
-		{"2+3", ast.BinOp{lex.OpPlus, ast.Integer(2), ast.Integer(3)}},
 		{
-			"2*3+4", ast.BinOp{
-				lex.OpPlus,
-				ast.BinOp{lex.OpStar, ast.Integer(2), ast.Integer(3)},
-				ast.Integer(4),
+			Name: "single number",
+			Expr: "42",
+			Want: ast.Integer(42),
+		},
+		{
+			Name: "single id",
+			Expr: "abc",
+			Want: "abc",
+		},
+		{
+			Name: "single binary operation",
+			Expr: "2+3",
+			Want: ast.BinOp{Op: lex.OpPlus, Left: ast.Integer(2), Right: ast.Integer(3)},
+		},
+		{
+			Name: "priority of multiplication over addition",
+			Expr: "2*3+4",
+			Want: ast.BinOp{
+				Op:    lex.OpPlus,
+				Left:  ast.BinOp{Op: lex.OpStar, Left: ast.Integer(2), Right: ast.Integer(3)},
+				Right: ast.Integer(4),
 			},
 		},
 		{
-			"2+3*4", ast.BinOp{
-				lex.OpPlus,
-				ast.Integer(2),
-				ast.BinOp{lex.OpStar, ast.Integer(3), ast.Integer(4)},
+			Name: "priority of multiplication over addition (but a bit different)",
+			Expr: "2+3*4",
+			Want: ast.BinOp{
+				Op:    lex.OpPlus,
+				Left:  ast.Integer(2),
+				Right: ast.BinOp{Op: lex.OpStar, Left: ast.Integer(3), Right: ast.Integer(4)},
 			},
 		},
 		{
-			"(2+3)*4", ast.BinOp{
-				lex.OpStar,
-				ast.BinOp{lex.OpPlus, ast.Integer(2), ast.Integer(3)},
-				ast.Integer(4),
+			Name: "parenthesis priority",
+			Expr: "(2+3)*4",
+			Want: ast.BinOp{
+				Op:    lex.OpStar,
+				Left:  ast.BinOp{Op: lex.OpPlus, Left: ast.Integer(2), Right: ast.Integer(3)},
+				Right: ast.Integer(4),
 			},
 		},
 		{
-			"-(+5)", ast.UnOp{
+			Name: "unary with expression in parenthesis",
+			Expr: "-(+5)",
+			Want: ast.UnOp{
 				Op: lex.UnMinus,
 				Value: ast.UnOp{
 					Op:    lex.UnPlus,
@@ -46,27 +68,44 @@ func TestParser(t *testing.T) {
 			},
 		},
 		{
-			"f(x, x+5)", ast.FCall{
+			Name: "pass expression via arguments",
+			Expr: "f(x, x+5)",
+			Want: ast.FCall{
 				Target: "f",
 				Args: []ast.Node{
-					"x", ast.BinOp{lex.OpPlus, "x", ast.Integer(5)},
+					"x", ast.BinOp{Op: lex.OpPlus, Left: "x", Right: ast.Integer(5)},
 				},
+			},
+		},
+		{
+			Name: "call the result of another function call",
+			Expr: "f(x)(y)",
+			Want: ast.FCall{
+				Target: ast.FCall{
+					Target: "f",
+					Args:   []ast.Node{"x"},
+				},
+				Args: []ast.Node{"y"},
 			},
 		},
 	}
 
 	for _, tc := range tcs {
-		testParser(t, tc.Expr, []ast.Node{tc.AST})
+		testParser(t, fmtTestName(tc.Name, tc.Expr), tc.Expr, []ast.Node{tc.Want})
 	}
 }
 
-func testParser(t *testing.T, code string, want ast.Program) {
+func testParser(t *testing.T, name, code string, want ast.Program) {
 	lexer := lex.NewLexer(code)
 	parser := NewParser(lexer)
 	tree, err := parser.Parse()
-	if !assert.NoError(t, err) {
+	if !assert.NoError(t, err, name) {
 		return
 	}
 
-	assert.Equal(t, want, tree)
+	assert.Equal(t, want, tree, name)
+}
+
+func fmtTestName(name, code string) string {
+	return fmt.Sprintf("%s (%s)", name, code)
 }
